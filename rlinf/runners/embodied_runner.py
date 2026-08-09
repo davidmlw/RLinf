8aa7810130ad8ceac60093e1ad8e6f664e7d2a1a
石乐同 <shiletong@outlook.com>
2026-08-09 12:55:10 +0800

    fix(runner): wait for set_global_step to complete before dispatching sync_model_to_rollout (#1443)
    Signed-off-by: shiletong <shiletong@infini-ai.com>
    Co-authored-by: shiletong <shiletong@infini-ai.com>

--- rlinf/runners/embodied_runner.py
+++ rlinf/runners/embodied_runner.py
@@ -494,8 +494,8 @@ class EmbodiedRunner:
         start_time = time.time()
         for _step in range(start_step, self.max_steps):
             # set global step
-            self.actor.set_global_step(self.global_step)
-            self.rollout.set_global_step(self.global_step)
+            self.actor.set_global_step(self.global_step).wait()
+            self.rollout.set_global_step(self.global_step).wait()
 
             profiled_step = (
                 self.global_step
@@ -602,8 +602,8 @@ class EmbodiedRunner:
         start_time = time.time()
         for _step in range(start_step, self.max_steps):
             # set global step
-            self.actor.set_global_step(self.global_step)
-            self.rollout.set_global_step(self.global_step)
+            self.actor.set_global_step(self.global_step).wait()
+            self.rollout.set_global_step(self.global_step).wait()
 
             profiled_step = (
                 self.global_step
